309ce668e44f54756f4654ba45dc70c9601aa782
David Junquero <djunquero@edu.tecnocampus.cat>
2020-02-20 19:36:21 +0100

    Up to extract_entities

--- task1.py
+++ task1.py
@@ -1,5 +1,5 @@
 from xml.dom.minidom import parse
-from nltk import tokenize
+from nltk.tokenize import word_tokenize
 import os
 
 '''
@@ -16,7 +16,12 @@ def nerc(inputdir, outputfile):
         sentences = tree.getElementsByTagName("sentence")
         for sentence in sentences:
             (sentence_id, text) = getSentenceInfo(sentence)
-            # tokenlist = tokenize(text)
+            tokenlist = tokenize(text)
+            entities = extract_entities(tokenlist)
+            output_entities(sentence_id, entities, outputfile)
+            for entity in entities:
+                print(str(entity))
+
 '''
         for sentence in tree:
             (sentence_id, text) = getSentenceInfo(sentence)
@@ -33,7 +38,34 @@ def getSentenceInfo(sentence):
     return sentence.getAttribute("id"), sentence.getAttribute("text")
 
 
-def tokenize(text):
+def tokenize(txt):
+    tokens = word_tokenize(txt)
+    offset = 0
+    for token in tokens:
+        offset = txt.find(token, offset)
+        yield token, offset, offset + len(token)
+        offset += len(token)
+
+
+def extract_entities(tokenlist):
+    entities = []
+    # Rule 1: words ending in "-ane" are drugs
+    for token in tokenlist:
+        if token[0].endswith("ane"):
+            entities.append(build_entity(token, "drug"))
+    return entities
+
+
+def build_entity(token, type):
+    entity = {
+        "name": token[0],
+        "offset": str(token[1]) + "-" + str(token[2]),
+        "type": type
+    }
+    return entity
+
+
+def output_entities(id, ents, outf):
     return
 
 
