74773011e8469a6d59db73d6b82dd54354d57202
David Junquero <djunquero@edu.tecnocampus.cat>
2020-02-27 16:27:14 +0100

    text file is cleared before reading

--- task1.py
+++ task1.py
@@ -64,7 +64,7 @@ def build_entity(token, type):
 
 
 def output_entities(sentence_id, entities, outputfile):
-    file = open(outputfile, "a")
+    file = open(outputfile, "w")
     for entity in entities:
         file.write(sentence_id + '|' + entity["offset"] + '|' + entity["name"] + '|' + entity["type"] + '\n')
 
